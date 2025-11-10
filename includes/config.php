81cc8a6d79ddd2cc979e1074d2d9a526a5b1c7ee
natwillia <natwillia@hartford.edu>
2025-11-09 20:58:02 -0500

    Add database credentials to config.php

---
+++ includes/config.php
@@ -0,0 +1,7 @@
+<?php
+// Constants for the credentials of the database in setup.sql
+
+const DBNAME = "student_passwords";
+const DBHOST = "localhost";
+const DBUSER = "passwords_user";
+const DBPASS = ""; // User has no password
