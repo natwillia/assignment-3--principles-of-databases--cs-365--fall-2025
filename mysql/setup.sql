ef235ab6ab1cbeb75800257d61f3bc486e356cb5
natwillia <natwillia@hartford.edu>
2025-11-09 20:34:18 -0500

    Update setup.sql

---
+++ mysql/setup.sql
@@ -0,0 +1,76 @@
+DROP DATABASE IF EXISTS student_passwords;
+
+CREATE DATABASE student_passwords;
+
+DROP USER IF EXISTS 'passwords_user'@'localhost';
+
+CREATE USER 'passwords_user'@'localhost';
+GRANT ALL ON student_passwords.* TO 'passwords_user'@'localhost';
+
+USE student_passwords;
+
+SET block_encryption_mode = 'aes-256-cbc';
+SET @key_str = UNHEX(SHA2('my secret passphrase', 512));
+SET @init_vector = RANDOM_BYTES(16);
+
+CREATE TABLE IF NOT EXISTS users (
+  user_id     INT               NOT NULL AUTO_INCREMENT,
+  username    VARCHAR(100)      NOT NULL,
+  first_name  VARCHAR(100)      NOT NULL,
+  last_name   VARCHAR(100)      NOT NULL,
+  email       VARCHAR(100)      NOT NULL,
+
+  PRIMARY KEY (user_id)
+);
+
+CREATE TABLE IF NOT EXISTS websites (
+  site_id    INT                NOT NULL AUTO_INCREMENT,
+  site_name  VARCHAR(100)       NOT NULL,
+  site_url   VARCHAR(255)       NOT NULL,
+
+  PRIMARY KEY (site_id)
+);
+
+CREATE TABLE IF NOT EXISTS accounts (
+  user_id      INT              NOT NULL,
+  site_id      INT              NOT NULL,
+  password     VARBINARY(512)   NOT NULL,
+  comment      VARCHAR(255),
+  time_stamp   TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+  PRIMARY KEY (user_id, site_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+  FOREIGN KEY (site_id) REFERENCES websites(site_id)
+);
+
+INSERT INTO users (username, first_name, last_name, email)
+VALUES
+  ('eschands', 'Edward', 'Scissorhands', 'ed3@gmail.com'),
+  ('fkrueger', 'Freddy', 'Krueger', 'fredkru10@gmail.com'),
+  ('leslion', 'Lestat', 'Lioncourt', 'lestatlion@gmail.com'),
+  ('gnftz', 'Ginger', 'Fitzgerald', 'gingfitz@gmail.com'),
+  ('mmfly', 'Marty', 'McFly', 'mcflymart@gmail.com');
+
+INSERT INTO websites (site_name, site_url)
+VALUES
+  ('eBay', 'https://www.ebay.com/'),
+  ('MGM Plus', 'https://www.mgmplus.com/'),
+  ('Letterboxd', 'https://letterboxd.com/'),
+  ('Food Network', 'https://www.foodnetwork.com/'),
+  ('Youtube', 'https://www.youtube.com/'),
+  ('Starz', 'https://www.starz.com/'),
+  ('Paramount Plus', 'https://www.paramountplus.com/'),
+  ('HBO Max', 'https://play.hbomax.com/');
+
+INSERT INTO accounts (user_id, site_id, password, comment)
+VALUES
+  (1, 1, AES_ENCRYPT('ebaypass1', @key_str, @init_vector), 'eBay login'),
+  (1, 2, AES_ENCRYPT('mgmsecure', @key_str, @init_vector), 'MGM Plus subscription'),
+  (2, 3, AES_ENCRYPT('letterboxd123', @key_str, @init_vector), 'Review movies'),
+  (2, 4, AES_ENCRYPT('foodpass', @key_str, @init_vector), 'Recipe ideas'),
+  (3, 5, AES_ENCRYPT('yt123', @key_str, @init_vector), 'YouTube login'),
+  (3, 6, AES_ENCRYPT('starzwatch', @key_str, @init_vector), 'Starz login'),
+  (4, 7, AES_ENCRYPT('paramountpw', @key_str, @init_vector), 'Paramount Plus login'),
+  (4, 8, AES_ENCRYPT('hbomaxpw', @key_str, @init_vector), 'HBO Max login'),
+  (5, 1, AES_ENCRYPT('shopping123', @key_str, @init_vector), 'marty eBay login'),
+  (5, 3, AES_ENCRYPT('letterb3', @key_str, @init_vector), 'marty letterboxd');
